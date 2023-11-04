f461b6d0678d4ebfb41f7b937c2402a27c877c54
tiin1705 <daonhattiin1705@gmail.com>
2023-11-04 14:14:20 +0700

    Update Destructible.cs

--- Assets/Scripts/Char/Destructible.cs
+++ Assets/Scripts/Char/Destructible.cs
@@ -4,22 +4,22 @@ public class Destructible : MonoBehaviour
 {
     public float destructionTime = 1f;
 
-    [Range(0f, 1f)]
-    public float itemSpawnChance = 0.2f;
-    public GameObject[] spawnableItems;
+    //[Range(0f, 1f)]
+  //  public float itemSpawnChance = 0.2f;
+   // public GameObject[] spawnableItems;
 
     private void Start()
     {
         Destroy(gameObject, destructionTime);
     }
 
-    private void OnDestroy()
+   /* private void OnDestroy()
     {
         if (spawnableItems.Length > 0 && Random.value < itemSpawnChance)
         {
             int randomIndex = Random.Range(0, spawnableItems.Length);
             Instantiate(spawnableItems[randomIndex], transform.position, Quaternion.identity);
         }
-    }
+    }*/
 
 }

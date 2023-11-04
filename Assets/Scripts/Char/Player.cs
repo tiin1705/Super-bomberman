1b9593c3147e46d5695d9a493c8be0caa063ffd3
tiin1705 <daonhattiin1705@gmail.com>
2023-11-04 12:27:23 +0700

    update animation, sorting layer, player. collision

--- Assets/Scripts/Char/Player.cs
+++ Assets/Scripts/Char/Player.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Players : MonoBehaviour
 {
     public float moveSpeed;
-    public Vector3 moveInput;
     private Animator animator;
+
+    public LayerMask HardObjectLayer;
+    public LayerMask SoftObjectLayer;
+    private bool isMoving;
+    private Vector2 input;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +26,30 @@ public class Players : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveInput();
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                var playerPos = transform.position;
+                playerPos.x += input.x * 0.5f;
+                playerPos.y += input.y * 0.5f;
+
+                if (isWalkable(playerPos))
+                    StartCoroutine(Move(playerPos));
+
+            }
+        }
+        animator.SetBool("isMoving", isMoving);
     }
 
-    void MoveInput()
+  /*  void MoveInput()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
@@ -53,7 +83,24 @@ public class Players : MonoBehaviour
         {
             animator.SetFloat("run-y", Mathf.Abs(moveInput.y));
         }
+    }*/
+
+    IEnumerator Move(Vector3 playerPos)
+    {
+        isMoving = true;
+        while ((playerPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, playerPos, 0.5f);
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+
+        }
+        transform.position = playerPos;
+        isMoving = false;
     }
+    
+
+    
     //   private void OnTriggerEnter2D(Collider2D collision)
     // { 
     //     string Objectname =collision.attachedRigidbody.gameObject.name ;
@@ -68,7 +115,32 @@ public class Players : MonoBehaviour
        
    // }
 
+    private bool checkHardObject(Vector3 playerPos)
+    {
+        if(Physics2D.OverlapCircle(playerPos, 0.01f, HardObjectLayer) != null)
+        {
+            return false;
+        }
+        return true;
+    }
+    private bool checkSoftObject(Vector3 playerPos)
+    {
+        if (Physics2D.OverlapCircle(playerPos, 0.01f, SoftObjectLayer) != null)
+        {
+            return false;
+        }
+        return true;
+    }
 
+    private bool isWalkable(Vector3 playerPos)
+    {
+        if(checkHardObject(playerPos) && checkSoftObject(playerPos))
+        {
+            return true;
+        }
+        return false;
+    }
+  
 
 
 }

43557f5346d1d35c79d5c84694bf492f1f264a5d
tiin1705 <daonhattiin1705@gmail.com>
2023-11-06 10:06:21 +0700

    commit

--- Assets/Scripts/Char/CameraFollow.cs
+++ Assets/Scripts/Char/CameraFollow.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target; // Tham chi?u ??n transform c?a "Camera Target"
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target); // ?? camera luôn nhìn vào "Camera Target"
     }
 }

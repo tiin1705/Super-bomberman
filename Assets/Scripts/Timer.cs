f430b84a3ce131c83ad06b7e88dce83964e0c0c4
dangduongkhang <dangduongkhang1406@gmail.com>
2023-11-14 17:39:45 +0700

    1

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class Timer : MonoBehaviour
+{
+  [SerializeField] TextMeshProUGUI timerText;
+  [SerializeField] float remainingTime;    
+
+  void Update()
+  {
+    if(remainingTime > 0)
+    {
+        remainingTime -= Time.deltaTime;
+    }
+    else if(remainingTime<0)
+    {
+        remainingTime = 0;
+        // GaneOver
+        timerText.color=Color.red;
+    }
+    
+    int minutes = Mathf.FloorToInt(remainingTime / 60);
+    int seconds = Mathf.RoundToInt(remainingTime % 60);
+    timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+  }
+}
